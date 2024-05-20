09662dda2fa6b41280f389bd5d5235cfac9e7664
ApurbaHasanJ <apurbahasanj@gmail.com>
2024-05-21 02:11:17 +0600

    project initialized

---
+++ src/app/config/index.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+// Load environment variables from the .env file
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+
+export default {
+  port: process.env.PORT || 5000,
+  database_url: process.env.DATABASE_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUND,
+};
