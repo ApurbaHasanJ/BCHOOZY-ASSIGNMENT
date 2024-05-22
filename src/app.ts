ac11f1d60865dfc589d0aa940667f8e895329cf0
ApurbaHasanJ <apurbahasanj@gmail.com>
2024-05-22 13:58:58 +0600

    default route added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from 'cors';
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import { ProductRoutes } from './app/modules/product/product.route';
 import { OrderRoutes } from './app/modules/order/order.route';
 const app: Application = express();
@@ -7,6 +7,24 @@ const app: Application = express();
 // parsers
 app.use(express.json());
 app.use(cors());
+
+// default route
+app.get('/', (req: Request, res: Response) => {
+    // Constructing server status object
+    const serverStatus = {
+      status: 'running',
+      message: 'BCHOOZY API is operational and running smoothly.',
+      timestamp: new Date().toISOString(),
+      version: 'v1.0.1',
+      uptime: process.uptime(),
+      author: 'Apurba Hasan J',
+      contact: {
+        email: 'apurbahasanj@gmail.com',
+        website: 'https://apurbahasanj.netlify.app/',
+      },
+    };
+    res.json(serverStatus);
+  });
 
 // application routes
 app.use('/api/products', ProductRoutes);
